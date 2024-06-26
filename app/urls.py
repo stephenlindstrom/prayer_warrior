07d7f36ab701a541b8f080e4146266f220520fdc
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-06-26 09:11:09 -0700

    Create new index view that both lists and creates items
    Uses get_context_data method to display all items for
    that user. Alters form_valid method to add user id to
    data that is posted

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,5 @@ app_name = "app"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("add_item/", views.AddItemView.as_view(), name="add_item"),
     path("register/", views.RegistrationView.as_view(), name="register"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.db.models.query import QuerySet
+from django.forms import BaseModelForm
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, Http404
 from django.contrib.auth.views import LoginView
@@ -13,29 +14,26 @@ from .forms import RegistrationForm
 from .models import Item
 
 
-class IndexView(LoginRequiredMixin, generic.ListView):
-    def get_queryset(self):
-        return Item.objects.filter(user=self.request.user)
-    
+class IndexView(LoginRequiredMixin, CreateView):
     model = Item
+    fields = ["item_name"]
     template_name = "app/index.html"
     login_url = reverse_lazy("login")
+    success_url = reverse_lazy("app:index")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["item_list"] = self.model.objects.filter(user=self.request.user)
+        return context
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class DetailView(generic.DetailView):
     model = Item
     template_name = "app/detail.html"
-
-
-class AddItemView(CreateView):
-    model = Item
-    fields = ["item_name"]
-    template_name = "app/index.html"
-
-# def add_item(request):
-#     new_item = request.POST["item"]
-#     Item.objects.create(item_name=new_item, user=request.user)
-#     return redirect(reverse("app:index"))
 
 
 class RegistrationView(SuccessMessageMixin, CreateView):
