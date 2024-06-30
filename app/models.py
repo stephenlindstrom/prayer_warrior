c0ef34054e7faad3d291368811ead9016117b29e
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-06-30 08:30:31 -0700

    Put title on base html so same for every page

--- app/models.py
+++ app/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class Item(models.Model):
-#     item_name = models.TextField()
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.item_name
     
 class PrayerRequest(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
