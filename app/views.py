04d820fb066afd81353a81bce997f90ed46def46
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-07-19 08:41:21 -0700

    Start pagination

--- app/views.py
+++ app/views.py
@@ -23,6 +23,7 @@ class PersonalPrayerView(LoginRequiredMixin, generic.ListView):
     model = PrayerRequest
     login_url = reverse_lazy("login")
     template_name = "app/personal-prayer.html"
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
